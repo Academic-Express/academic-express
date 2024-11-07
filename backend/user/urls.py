f1035a24aa52bc4df8596ec2e7f62238e7677dfe
hotwords123 <hotwords123@gmail.com>
2024-11-07 22:03:39 +0800

    feat(backend): remove trailing slashes in api urls

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -6,11 +6,11 @@ from . import views
 
 app_name = 'user'
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('login/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('profile/', views.UserView.as_view(), name='get_current_user'),
-    path('profile/<int:pk>/', views.get_user_by_id, name='get_user_by_id'),
-    path('change-password/', views.change_password, name='change_password'),
+    path('register', views.register, name='register'),
+    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/verify', TokenVerifyView.as_view(), name='token_verify'),
+    path('profile', views.UserView.as_view(), name='get_current_user'),
+    path('profile/<int:pk>', views.get_user_by_id, name='get_user_by_id'),
+    path('change-password', views.change_password, name='change_password'),
 ]
