b45b98c2ba3e396d2979fcf4ae2d48f36f81884b
hotwords <31656766+hotwords123@users.noreply.github.com>
2024-11-19 15:56:01 +0800

    feat(frontend/pub): add api to get arxiv entry (#17)

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -56,12 +56,35 @@ export interface PatchProfileRequest {
   phone: string
 }
 
+export interface ArxivEntry {
+  arxiv_id: string
+  title: string
+  summary: string
+  authors: ArxivAuthor[]
+  comment: string
+  published: string
+  updated: string
+  primary_category: string
+  categories: string[]
+  link: string
+  pdf: string
+  slug: string
+  view_count: number
+  citation_count: number
+}
+
+export interface ArxivAuthor {
+  name: string
+  affiliation?: string
+}
+
 export const URLS = {
   login: '/v1/user/login',
   refreshLogin: '/v1/user/login/refresh',
   register: '/v1/user/register',
   getCurrentUser: '/v1/user/profile',
-  getUserById: (id: number) => `/v1/user/profile/${id}`,
+  getUserById: (userId: number) => `/v1/user/profile/${userId}`,
+  getArxivEntry: (arxivId: string) => `/v1/pub/arxiv/${arxivId}`,
 }
 
 export function login(payload: LoginRequest) {
@@ -80,10 +103,14 @@ export function getCurrentUser() {
   return client.get<UserDetail>(URLS.getCurrentUser)
 }
 
-export function getUserById(id: number) {
-  return client.get<User>(URLS.getUserById(id))
+export function getUserById(userId: number) {
+  return client.get<User>(URLS.getUserById(userId))
 }
 
 export function patchProfile(payload: PatchProfileRequest) {
   return client.patch<UserDetail>(URLS.getCurrentUser, payload)
+}
+
+export function getArxivEntry(arxivId: string) {
+  return client.get<ArxivEntry>(URLS.getArxivEntry(arxivId))
 }
