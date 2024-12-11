90b8cef0218793cb2a0845f2b263c11202c29197
hotwords123 <hotwords123@gmail.com>
2024-12-11 19:59:50 +0800

    feat(frontend/userhistory): add timeline for history view

--- frontend/src/components/user/HistoryPanel.vue
+++ frontend/src/components/user/HistoryPanel.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import { useI18n } from 'vue-i18n'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/zh-cn'
 
 import ArxivItem from '../feed/ArxivItem.vue'
 import GithubItem from '../feed/GithubItem.vue'
@@ -11,7 +14,31 @@ import {
   type History,
 } from '@/services/api'
 
+dayjs.extend(relativeTime).locale('zh-cn')
+
 const historyItems = ref<History[]>([])
+const timelineItems = computed(() => {
+  // Group history items by viewed_at.
+  const groupedItems = historyItems.value.reduce(
+    (acc, item) => {
+      const viewedAt = dayjs(item.viewed_at).fromNow()
+      if (!acc[viewedAt]) {
+        acc[viewedAt] = []
+      }
+      acc[viewedAt].push(item)
+      return acc
+    },
+    {} as Record<string, History[]>,
+  )
+
+  groupedItems[''] = []
+
+  // Convert grouped items to timeline items.
+  return Object.entries(groupedItems).map(([viewedAt, items]) => ({
+    viewed_at: viewedAt,
+    items,
+  }))
+})
 
 const { t } = useI18n()
 
@@ -39,35 +66,63 @@ const deleteHistoryItem = async (id: number) => {
     <p>{{ t('historyNotFound') }}</p>
   </template>
   <template v-else>
-    <template v-for="(item, index) in historyItems" :key="index">
-      <div class="flex flex-wrap">
-        <p class="flex items-center">
-          {{ new Date(item.viewed_at).toLocaleString() }}
-        </p>
-        <div class="ml-auto flex items-center">
-          <Button
-            icon="pi pi-trash"
-            variant="text"
-            severity="secondary"
-            @click="deleteHistoryItem(item.id)"
-          ></Button>
-        </div>
-      </div>
+    <Timeline class="history" :value="timelineItems" align="left">
+      <template #opposite="{ item }">
+        <p class="text-right">{{ item.viewed_at }}</p>
+      </template>
 
-      <ArxivItem
-        v-if="item.content_type === FeedOrigin.Arxiv"
-        :arxivEntry="item.entry_data"
-      />
-      <GithubItem
-        v-else-if="item.content_type === FeedOrigin.Github"
-        :githubRepo="item.entry_data"
-      />
-    </template>
+      <template #content="{ item: { items } }">
+        <template v-if="items.length === 0">
+          <p class="text-center">{{ t('endOfTimeline') }}</p>
+        </template>
+
+        <template v-for="item in items" :key="item.id">
+          <div class="flex">
+            <div class="mr-4 flex-1">
+              <ArxivItem
+                v-if="item.content_type === FeedOrigin.Arxiv"
+                :arxivEntry="item.entry_data"
+              />
+              <GithubItem
+                v-else-if="item.content_type === FeedOrigin.Github"
+                :githubRepo="item.entry_data"
+              />
+            </div>
+
+            <div class="flex flex-col justify-center">
+              <Button
+                class="hover:!text-red-600"
+                icon="pi pi-trash"
+                variant="text"
+                severity="secondary"
+                @click="deleteHistoryItem(item.id)"
+              ></Button>
+              <Button
+                class="hover:!text-blue-600"
+                icon="pi pi-star"
+                variant="text"
+                severity="secondary"
+              ></Button>
+            </div>
+          </div>
+
+          <hr class="my-4" />
+        </template>
+      </template>
+    </Timeline>
   </template>
 </template>
 
+<style scoped>
+.history :deep(.p-timeline-event-opposite) {
+  flex: 0;
+  min-width: 100px;
+}
+</style>
+
 <i18n locale="zh-CN">
 {
-  "historyNotFound": "未找到历史记录",  
+  "historyNotFound": "未找到历史记录",
+  "endOfTimeline": "没有更多记录了",
 }
 </i18n>
